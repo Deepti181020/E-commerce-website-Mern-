82bd5fad1504918b2d11ec70e1bd7f50ff99036a
Deepti lata Ekka <deeptilata18@gmail.com>
2024-04-19 15:37:44 +0530

    Initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,21 +5,23 @@ import cart_icon from '../Asset/cart_icon.png'
 
 const Navbar = () => {
   return (
-    <div className='navbar flex justify-around p-4 shadow-xl bg-slate-300 '>
+    <div className='navbar flex justify-around p-10 shadow-xl bg-slate-300 '>
        <div className="nav-logo flex items-center gap-10">
             <img src={logo} alt="Error" />
             <p className='text-4xl font-semibold'>SHOPAHOLIC</p>
         </div> 
         <ul className="nav-menu flex items-center list-none text-base font-medium">
-            <li className='menu-list flex flex-col items-center justify-center gap-1'>Home < hr/></li>
+            <li className='menu-list flex flex-col items-center justify-center gap-1'>Home 
+            < hr id="hr1"/>
+            </li>
             <li className='menu-list flex flex-col items-center justify-center gap-1'>Shop</li>
             <li className='menu-list flex flex-col items-center justify-center gap-1'>Men</li>
             <li className='menu-list flex flex-col items-center justify-center gap-1'>Women</li>
             <li className='menu-list flex flex-col items-center justify-center gap-1'>Kids</li>  
         </ul>
 
-        <div className="nav-login-cart">
-            <button>Login</button>
+        <div className="nav-login-cart flex items-center gap-32">
+            <button className='w-36 h-14 outline-none text-slate-800  text-base font-medium  hover:bg-slate-400'>Login</button>
             <img src={cart_icon} alt="" />
         </div>
     </div>
